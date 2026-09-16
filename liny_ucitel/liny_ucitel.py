c03eee7b8a6b88b13f544a7a61ace4224f17847b
Jakub Štěpánek <jakub14step@seznam.cz>
2026-09-16 09:58:00 +0200

    dodělání líného učitele

--- liny_ucitel/liny_ucitel.py
+++ liny_ucitel/liny_ucitel.py
@@ -1,6 +1,6 @@
 import random
 import json
-
+import tkinter
 with open("H:\programovani25\PRG_4.G\liny_ucitel\zaci.json", "r", encoding="utf-8") as f:
     zaci = json.load(f)
     print(f"Loadnuto: {zaci}")
@@ -15,24 +15,62 @@ zak = input("čí to bude známka?")
 #     "radek": 5,
 
 # }
-if not zak in zaci:
-    pridat = input("Mámn tohoto žáka přidat do seznamu? ano/ne       ")
+def zapsat_1(pridat):
     if pridat == "ano":
-        zaci.update({zak: nahodna_znamka})
+        # zaci.update({zak.append(nahodna_znamka)})
+        zaci[zak].append(nahodna_znamka)
         print(zaci)
         with open("H:\programovani25\PRG_4.G\liny_ucitel\zaci.json", "w", encoding="utf-8") as f:
             json.dump(zaci, f, ensure_ascii=False, indent = 4)
             print("uloženo")
     else: 
         pass
+def zapsat_2(pridat):
+    if pridat == "ano":
+        zaci.update({zak: [nahodna_znamka]})
+        # zaci[zak].append(nahodna_znamka)
+        print(zaci)
+        with open("H:\programovani25\PRG_4.G\liny_ucitel\zaci.json", "w", encoding="utf-8") as f:
+            json.dump(zaci, f, ensure_ascii=False, indent = 4)
+            print("uloženo")
+    else: 
+        pass
+if zak == "mohamed":
+    nahodna_znamka = int(input("jaká bude mohamedova známka?"))
+    pridat = input("Mámn tohoto žáka přidat do seznamu? ano/ne       ")
+    if not zak in zaci:
+        zapsat_2(pridat)
+    else:
+        zapsat_1(pridat)
+elif not zak in zaci:
+    pridat = input("Mámn tohoto žáka přidat do seznamu? ano/ne       ")
+    zapsat_2(pridat)
 
 elif zak in zaci:
     if zaci[zak] == nahodna_znamka:
-        print(f"Žák {zak} má již tuto známku({nahodna_znamka})")
+        # print(f"Žák {zak} má již tuto známku({nahodna_znamka})")
+        pridat = input("Mámn tohoto žáka přidat do seznamu? ano/ne       ")
+        zapsat_1(pridat)
+
     else:
-        print(f"Snažíte se přidělit žákovi {zak} znamku {nahodna_znamka} přestože již má {zaci[zak]}")
+        # print(f"Snažíte se přidělit žákovi {zak} znamku {nahodna_znamka} přestože již má {zaci[zak]}")
+        pridat = input("Mámn tohoto žáka přidat do seznamu? ano/ne       ")
+        zapsat_1(pridat)
 else:
     print("Spadly bakaláře")
+
+prumer = zaci[zak]
+soucet = 0
+pocet = 0
+for i in prumer:
+    print(prumer)
+    soucet += i
+    pocet +=1
+vysledek = soucet/pocet
+if vysledek >3:
+    print("smutné")
+print(f"vysledek je: {vysledek}")
+
 
 
 
